9651620385011f98c20adc5b655d24c12e00a823
jgcodes2020 <jackyguo18@hotmail.com>
2022-05-06 16:29:15 -0400

    finish main UI builder xml is pain

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,36 @@
 #include <cstdlib>
 #include <iostream>
-#include "ButtonFrame.hpp"
 #include "gdkmm/display.h"
-#include "glibmm/ustring.h"
-#include "gtkmm/button.h"
-#include "gtkmm/cssprovider.h"
-#include "gtkmm/enums.h"
-#include "gtkmm/stylecontext.h"
-#include "gtkmm/togglebutton.h"
-#include "sigc++/functors/ptr_fun.h"
+#include "gtkmm/box.h"
+#include "gtkmm/builder.h"
 #include <sigc++/sigc++.h>
 #include <gtkmm.h>
+
+#include "gtkmm/cssprovider.h"
+#include "gtkmm/stylecontext.h"
+#include <resources/ui/main.ui.rsrc.hpp>
+#include <resources/css/main.css.rsrc.hpp>
 
 namespace tasdi2 {
   class MainWindow : public Gtk::Window {
   public:
-    MainWindow() :
-      frame(),
-      main_box(Gtk::Orientation::VERTICAL) {
-      // Setup box
-      main_box.set_homogeneous(true);
-      main_box.set_margin(10);
-      main_box.append(frame);
-      
-      tbt = Gtk::Button("Hello!");
-      main_box.append(tbt);
-      
-      set_child(main_box);
+    MainWindow() : builder(Gtk::Builder::create_from_string(tasdi2::rsrc::ui_data)) {
+      set_child(*builder->get_widget<Gtk::Box>("main-root"));
     };
   protected:
-    // Container
-    Gtk::Box main_box;
-    // Widgets
-    ButtonFrame frame;
-    Gtk::Button tbt;
+    Glib::RefPtr<Gtk::Builder> builder;
   };
 }
 
 int main(int argc, char* argv[]) {
   auto app = Gtk::Application::create("io.github.jgcodes2020.testapp");
+  app->signal_startup().connect([]() {
+    auto css_loader = Gtk::CssProvider::create();
+    css_loader->load_from_data(tasdi2::rsrc::css_data);
+    
+    auto display = Gdk::Display::get_default();
+    
+    Gtk::StyleContext::add_provider_for_display(display, css_loader, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+  });
   return app->make_window_and_run<tasdi2::MainWindow>(argc, argv);
 }
